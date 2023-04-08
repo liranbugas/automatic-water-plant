d1af9990c3fcd85d743fddac44c16e3e2305c10c
liranbugas <122556538+liranbugas@users.noreply.github.com>
2023-04-08 19:09:42 +0300

    Add files via upload

---
+++ Core/Inc/soil_sensor.h
@@ -0,0 +1,14 @@
+/*
+ * soil_sensor.h
+ *
+ *  Created on: 8 Apr 2023
+ *      Author: liran
+ */
+#include <stdio.h>
+#ifndef SRC_SOIL_SENSOR_H_
+#define SRC_SOIL_SENSOR_H_
+uint16_t  humidity = 0;
+#define pull_pump  4000
+#define wait_time  10000
+
+#endif /* SRC_SOIL_SENSOR_H_ */
